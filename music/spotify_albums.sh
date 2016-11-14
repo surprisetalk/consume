f392bf02d30f8e86d95d72b88e7b5831f6d5013f
Taylor Sarrafian <surprisetalk@gmail.com>
2016-11-14 10:59:08 -0800

    dickin around

--- music/spotify_albums.sh
+++ music/spotify_albums.sh
@@ -1,6 +1,14 @@
-#!/bin/racket
+#! /bin/racket
+#lang racket
 
-# make a racket script that merges my spotify albums into an org file
+(require json)
 
-# https://developer.spotify.com/web-api/console/get-current-user-saved-albums/?limit=50
+;; https://developer.spotify.com/web-api/console/get-current-user-saved-albums/?limit=50
+;; curl -X GET "https://api.spotify.com/v1/me/albums" -H "Accept: application/json" -H "Authorization: Bearer BQBW6TED3edkJKwfRaEEKmVxuKzWqurtH02LZRqaywrxzQxMJJD_WDGDEXbSILNcOxrjrC7EAf67KEn5tfGKoZfkJyacrz3RGoMvfTKWdbS3zqL2ex7qr781F4WfHW-ehbJbIWVk_ck3uWG5YERAJn0l1e9MSdunv_1uvWghZCAmDYKorgMZkrWSw3C3KyLeWCylcoh63hmNaVUDfAYJIwyR4x-G2X0fWZpIN5zvGymEMBIx7UuOGfHIdjQ5b5YeouiWIV1BoHKwN3SA97Rb9I6NqRDtDE5U0ZO3ZM-B_mcYT4KgE85P4HSA-Q"
+
+;; (define (fetch-json str)
+;;   (read-json (get-pure-port (string->url str))))
+
+;; TODO: wishful thinking
+
 
